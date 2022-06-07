be76b86335ca07d24a04582fbfabf5eb1076dd7e
pascalPeuch <pascal.peuch@outletcity.com>
2022-06-07 15:43:01 +0200

    Initial Commit

---
+++ NavigationStack/NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackApp.swift
+//  NavigationStack
+//
+//  Created by admin-pp on 07.06.22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
